29b2a60cbaccf44266b56b096724f4a66f6f0f96
cxy <changclear@163.com>
2020-02-19 16:19:35 +0800

    add add delete

--- hello.py
+++
@@ -1,7 +0,0 @@
-print("Hello world")
-print('test branch')
-<<<<<<< HEAD
-print("Creating a new branch is quick AND simple.")
-=======
-print("Creating a new branch is quick AND simple.")
->>>>>>> feature1
